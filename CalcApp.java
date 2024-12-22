a5b9b46cfc21b257f2b1d4ff89c4c443af39c6b3
cdwilkie <cdwilkie@aggies.ncat.edu>
2024-12-22 18:57:59 -0500

    Update CalcApp.java
    Placed GUI creation inside invokeLater method.

--- CalcApp.java
+++ CalcApp.java
@@ -1,10 +1,14 @@
+import javax.swing.*;
+
 public class CalcApp {
 
     public static void main(String[] args) {
         CalcView testCalc = new CalcView();
         CalcModel testModel = new CalcModel();
         CalcController calcController = new CalcController(testCalc, testModel);
-        calcController.getCalcView().runDemo();
+        SwingUtilities.invokeLater(() -> {
+            calcController.getCalcView().runDemo();
+        });
     }
     
 }
