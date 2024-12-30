8c3f5a84d58dffabf5f8e080eebcd717a7a27f42
cdwilkie <cdwilkie@aggies.ncat.edu>
2024-12-30 18:45:33 -0500

    Formatted Test Driver class
    Removed text color codes and updated API call.

--- unitTests/TestDriver.java
+++ unitTests/TestDriver.java
@@ -2,16 +2,9 @@ package unitTests;
 
 public class TestDriver {
 
-    // Textcolor Modifiers for Console Output
-    // Example: System.out.println(RED + "This is red text" + RESET);
-    public static final String GREEN = "\033[0;32m";
-    public static final String RED = "\033[0;31m";
-    public static final String RESET = "\033[0m";
-    public static final String YELLOW = "\033[0;33m";
-
     public static void main(String[] args) {
         ModelTest mt = new ModelTest();
-        mt.test();
+        mt.runTest();
     }
     
     
